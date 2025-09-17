36c8626859e463f19fd0f386e2ec16b149839056
Umer-Jahangir <umerjahangir.cs@gmail.com>
2025-09-17 17:10:49 +0500

    add environment variables

--- Portfolio/react/src/sanityClient.js
+++ Portfolio/react/src/sanityClient.js
@@ -2,11 +2,12 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "qu3xgaxc", // find in sanity.json or sanity.config
-  dataset: "production",
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
   apiVersion: "2023-01-01",
   useCdn: true,
 });
+
 
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
